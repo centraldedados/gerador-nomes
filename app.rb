f3af369fdeb5ada60d0e5108337e8310ce651b1d
Joao Antunes <joao.antunes@me.com>
2019-04-14 23:22:03 +0100

    encoding fix

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 # encoding: utf-8
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 require 'sinatra'
 require 'sinatra/reloader'
 require "sinatra/multi_route"
